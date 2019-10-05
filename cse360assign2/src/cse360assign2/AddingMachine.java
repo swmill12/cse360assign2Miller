35070597284921ca6e245e6d58534cab60ff6ea0
Shaun <swmill12@asu.edu>
2019-10-05 13:38:00 -0700

    Added functionality to all methods and tested.

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -3,28 +3,32 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
-	
+	private String output;
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		output = "0";
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		total = total + value;
+		output = output + " + " + value;
 	}
 	
 	public void subtract (int value) {
-		
+		total = total - value;
+		output = output + " - " + value;
 	}
 		
 	public String toString () {
-		return "";
+		return output;
 	}
 
 	public void clear() {
-	
+		total = 0;
+		output = "0";
 	}
 }
